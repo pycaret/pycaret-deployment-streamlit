fd9694f2bf47dad2c22ae5a2c130aba138a1ea73
PyCaret <moez@pycaret.org>
2020-06-16 22:25:37 -0400

    updated setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 mkdir -p ~/.streamlit/
-echo “\
+echo "\
 [general]\n\
-email = \”your-email@domain.com\”\n\
-“ > ~/.streamlit/credentials.toml
-echo “\
+email = \"your-email@domain.com\"\n\
+" > ~/.streamlit/credentials.toml
+echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
-“ > ~/.streamlit/config.toml
+" > ~/.streamlit/config.toml
